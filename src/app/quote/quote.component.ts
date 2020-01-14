6d35d8f421ce4c72c18f60d6eb13eb92fc69ae63
kraft-owaka <owakakraft@gmail.com>
2020-01-14 12:19:12 +0300

    added adescription to the goquotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,13 @@ import { Quote } from'../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote []= [
-    {id:1, name:'Watch finding Nemo'},
-    {id:2,name:'Buy Cookies'},
-    {id:3,name:'Get new Phone Case'},
-    {id:4,name:'Get Dog Food'},
-    {id:5,name:'Solve math homework'},
-    {id:6,name:'Plot my world domination plan'},
+    {id:1, name:'Watch finding Nemo',description:'Find an online version and watch merlin find his son'},
+    {id:2,name:'Buy Cookies',description:'Find an online version and watch merlin find his son'},
+    {id:3,name:'Get new Phone Case',description:'Find an online version and watch merlin find his son'},
+    {id:4,name:'Get Dog Food',description:'Find an online version and watch merlin find his son'},
+    {id:4,name:'Get Dog Food',description:'Find an online version and watch merlin find his son'},
+    {id:5,name:'Solve math homework',description:'Find an online version and watch merlin find his son'},
+    {id:6,name:'Plot my world domination plan',description:'Find an online version and watch merlin find his son'},
   ];
 
   constructor() { }
